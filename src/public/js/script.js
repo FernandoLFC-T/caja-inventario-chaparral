5d1b54bb3281f5f1a7d4b71e33445da1bfa505fd
FernandoLFC-T <ingffc@gmail.com>
2026-05-19 19:14:09 -0500

    ActualizacionOMEN

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -9,29 +9,20 @@ $(function() {
         const $form = $(this);
         const $button = $form.find('button');
         const $state = $button.find('.state');
-
-        // 1. Iniciar animación de carga en el botón
+        
         $button.addClass('loading');
         $state.html('Autenticando...');
-        
-        // --- Simulación de conexión al servidor (2 segundos) ---
-        // Pronto lo conectaremos a la Base de Datos real
+
         setTimeout(function() {
             
             // 2. Transición al estado de ÉXITO
-            
-            // El botón se vuelve verde con un 'pop' (transform: scale) en CSS
             $button.removeClass('loading').addClass('ok');
             $state.html('¡Bienvenido! El Chaparral');
             
-            // El formulario se expande suavemente (gracias a transition: all en .login)
             $form.addClass('ok'); 
 
             // 3. Simular Redirección (tras ver la animación de bienvenida)
             console.log("Credenciales validadas correctamente. Redirigiendo...");
-            setTimeout(() => {
-                // window.location.href = "/dashboard"; 
-
-        }, 2000); // 2 segundos de carga simulada
+                window.location.href = "/caja.html";
     });
 });})
